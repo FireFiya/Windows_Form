90a3ae4fd9cf57183bb9b7b679b2891b5a0a71ba
FireFiya <mark20040810@gmail.com>
2023-07-06 18:26:25 +0800

    little update

--- WFTest/Properties/AssemblyInfo.cs
+++ WFTest/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("WFTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("本檔案是為學習C#與Winform")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WFTest")]
