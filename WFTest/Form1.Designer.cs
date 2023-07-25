f9c83088f8b6279e43d0da389398c158ac664792
FireFiya <mark20040810@gmail.com>
2023-07-25 15:58:08 +0800

    a test for all objects position

--- WFTest/Form1.Designer.cs
+++ WFTest/Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(893, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(960, 540);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "BlackJack";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- WFTest/Form1.cs
+++ WFTest/Form1.cs
@@ -12,17 +12,63 @@ namespace WFTest
 {
     public partial class Form1 : Form
     {
-
-
-
+        private float X;//當前窗體的寬度
+        private float Y;//當前窗體的高度
+        bool isLoaded;  // 是否已設定各控制的尺寸資料到Tag屬性
+        /// <summary>
+        /// 將控制項的寬，高，左邊距，頂邊距和字體大小暫存到tag屬性中
+        /// </summary>
+        /// <param name="cons">遞歸控制項中的控制項</param>
+        private void SetTag(Control cons)
+        {
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ":" + con.Height + ":" + con.Left + ":" + con.Top + ":" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                    SetTag(con);
+            }
+        }
+        private void SetControls(float newx, float newy, Control cons)
+        {
+            if (isLoaded)
+            {
+                //遍歷窗體中的控制項，重新設置控制項的值
+                foreach (Control con in cons.Controls)
+                {
+                    string[] mytag = con.Tag.ToString().Split(new char[] { ':' });//獲取控制項的Tag屬性值，並分割後存儲字元串數組
+                    float a = System.Convert.ToSingle(mytag[0]) * newx;//根據窗體縮放比例確定控制項的值，寬度
+                    con.Width = (int)a;//寬度
+                    a = System.Convert.ToSingle(mytag[1]) * newy;//高度
+                    con.Height = (int)(a);
+                    a = System.Convert.ToSingle(mytag[2]) * newx;//左邊距離
+                    con.Left = (int)(a);
+                    a = System.Convert.ToSingle(mytag[3]) * newy;//上邊緣距離
+                    con.Top = (int)(a);
+                    Single currentSize = System.Convert.ToSingle(mytag[4]) * newy;//字體大小
+                    con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                    if (con.Controls.Count > 0)
+                    {
+                        SetControls(newx, newy, con);
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X = this.Width;//獲取窗體的寬度
+            Y = this.Height;//獲取窗體的高度
+            isLoaded = true;// 已設定各控制項的尺寸到Tag屬性中
+            SetTag(this);//調用方法
+        }
         public Form1()
         {
             InitializeComponent();
+            isLoaded = false;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Shown(object sender, EventArgs e)
         {
-            WindowState = FormWindowState.Maximized;
+            this.WindowState = FormWindowState.Maximized;
         }
     }
 }
